13853f3e8731a2ddb38bbadc726d9f297105f58a
Thirumurugan10 <thiruthiva10@gmail.com>
2025-06-29 18:41:13 +0530

    first vistion

---
+++ src/layout/Navbar.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+export default function
+    () {
+    return (
+        <div>
+
+
+            <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
+                <div className="container-fluid">
+                    <Link className="navbar-brand" to={"/"}>
+                    Full Stack Application</Link>
+                    
+
+                    <Link className="btn btn-outline-light" to="/adduser">Add User</Link>
+
+                </div>
+            </nav>
+            
+
+        </div>
+    )
+}
